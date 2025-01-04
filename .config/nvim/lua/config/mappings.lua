d6339e003466fe53c45c843d9422e99f29cf2ce5
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-01-04 23:25:50 +0600

    adding some keymap for nvim

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -1,4 +1,16 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>ee", vim.cmd.Ex)
+keymap.set("n", "<leader>ch", ":nohl<CR>")
+
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>sq", "<cmd>close<CR>")
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>") 
+keymap.set("n", "<leader>tq", "<cmd>tabclose<CR>") 
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>") 
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>") 
